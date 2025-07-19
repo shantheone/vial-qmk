f9e84430a8fa0b5d852265a24a8c80b59072a087
shantheone <shandoor@gmail.com>
2025-07-19 09:15:37 +0200

    Added handedness

---
+++ keyboards/beekeeb/3w6hs/keymap.c
@@ -0,0 +1,1 @@
+const char chordal_hold_layout[MATRIX_ROWS][MATRIX_COLS] PROGMEM = LAYOUT('L', 'L', 'L', 'L', 'L', 'L', 'R', 'R', 'R', 'R', 'R', 'R', 'L', 'L', 'L', 'L', 'L', 'L', 'R', 'R', 'R', 'R', 'R', 'R', 'L', 'L', 'L', 'L', 'L', 'L', 'R', 'R', 'R', 'R', 'R', 'R', '*', '*', '*', '*', '*', '*');
