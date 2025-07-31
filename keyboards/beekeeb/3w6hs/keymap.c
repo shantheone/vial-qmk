359fc3e71251178a7200ecae6c3c48277d9fd863
shantheone <shandoor@gmail.com>
2025-07-31 11:12:42 +0200

    Removed keymap.c handedness config

--- keyboards/beekeeb/3w6hs/keymap.c
+++
@@ -1,1 +0,0 @@
-const char chordal_hold_layout[MATRIX_ROWS][MATRIX_COLS] PROGMEM = LAYOUT('L', 'L', 'L', 'L', 'L', 'L', 'R', 'R', 'R', 'R', 'R', 'R', 'L', 'L', 'L', 'L', 'L', 'L', 'R', 'R', 'R', 'R', 'R', 'R', 'L', 'L', 'L', 'L', 'L', 'L', 'R', 'R', 'R', 'R', 'R', 'R', '*', '*', '*', '*', '*', '*');
